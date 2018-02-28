b740c29cf4c4f8d0f799155fe57e36c482e23d0c
WWK563388548 <wc563388548@gmail.com>
2018-02-28 23:52:13 +0900

    add function of register part1

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// passport figuration
+app.use(require("express-session")({
+    secret: "wwk4652218",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Create data
 /*Restaurant.create(
     {
@@ -137,6 +149,25 @@ app.post("/restaurants/:id/comments", function(req, res){
                 }
             });
         }
+    });
+});
+
+// Add auth route
+// add register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+// handle register logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/restaurants");
+        });
     });
 });
 
