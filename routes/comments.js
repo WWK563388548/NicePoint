e1ce0578600a6a9b5588121fb282312f9584f900
WWK563388548 <wc563388548@gmail.com>
2018-03-04 20:29:41 +0900

    add function of  comment delete

--- routes/comments.js
+++ routes/comments.js
@@ -69,6 +69,18 @@ router.put("/:comment_id", function(req, res){
     });
 });
 
+// comment delete route
+router.delete("/:comment_id", function(req, res){
+    //findByIdAndRemove
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+       if(err){
+           res.redirect("back");
+       } else {
+           res.redirect("/restaurants/" + req.params.id);
+       }
+    });
+});
+
 // middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
