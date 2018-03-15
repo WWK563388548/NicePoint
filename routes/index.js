1158e5de6a073db2c1f6aac6c3bd1b6c3a9d0d42
WWK563388548 <wc563388548@gmail.com>
2018-03-16 01:42:48 +0900

    Added function of forgot and reset password by email

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 var Restaurant = require("../models/restaurant");
+var async = require("async");
+var nodemailer = require("nodemailer");
+var crypto = require("crypto");
 
 // Create a landing page
 // root route
@@ -58,6 +61,125 @@ router.get("/logout", function(req, res){
     req.flash("success", "您已退出登陆");
     res.redirect("/restaurants");
 });
+
+// forgot password
+router.get('/forgot', function(req, res) {
+      res.render('forgot');
+});
+    
+    router.post('/forgot', function(req, res, next) {
+      async.waterfall([
+        function(done) {
+          crypto.randomBytes(20, function(err, buf) {
+            var token = buf.toString('hex');
+            done(err, token);
+          });
+        },
+        function(token, done) {
+          User.findOne({ email: req.body.email }, function(err, user) {
+            if (!user) {
+              req.flash('error', '该注册邮箱不存在');
+              return res.redirect('/forgot');
+            }
+    
+            user.resetPasswordToken = token;
+            user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+    
+            user.save(function(err) {
+              done(err, token, user);
+            });
+          });
+        },
+        function(token, user, done) {
+          var smtpTransport = nodemailer.createTransport({
+            service: 'Gmail', 
+            auth: {
+              user: 'NicePointInfo@gmail.com',
+              pass: "wwk4652218"
+            }
+          });
+          // NicePointOfficialInfo@gmail.com
+          var mailOptions = {
+            to: user.email,
+            from: 'NicePointInfo@gmail.com',
+            subject: 'NicePoint 密码重制',
+            text: '您能收到这封邮件是因为您已经申请了密码重置功能。\n\n' +
+              '请点击以下链接，或者在浏览器中复制粘贴该链接来完成接下来的步骤：\n\n' +
+              'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+              '如果此次密码重置不是由您本人申请，请无视此邮件，您的密码也将不会被改变。\n'
+          };
+          smtpTransport.sendMail(mailOptions, function(err) {
+            console.log('mail sent');
+            req.flash('success', '一封邮件已经被发往 ' + user.email + ' ，请查收。');
+            done(err, 'done');
+          });
+        }
+      ], function(err) {
+        if (err) return next(err);
+        res.redirect('/forgot');
+      });
+    });
+    
+    // reset password by email
+    router.get('/reset/:token', function(req, res) {
+      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'Password reset token不可用或已经失效');
+          return res.redirect('/forgot');
+        }
+        res.render('reset', {token: req.params.token});
+      });
+    });
+    
+    router.post('/reset/:token', function(req, res) {
+      async.waterfall([
+        function(done) {
+          User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+            if (!user) {
+              req.flash('error', 'Password reset token不可用或已经失效');
+              return res.redirect('back');
+            }
+            if(req.body.password === req.body.confirm) {
+              user.setPassword(req.body.password, function(err) {
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
+    
+                user.save(function(err) {
+                  req.logIn(user, function(err) {
+                    done(err, user);
+                  });
+                });
+              })
+            } else {
+                req.flash("error", "密码不匹配");
+                return res.redirect('back');
+            }
+          });
+        },
+        function(user, done) {
+          var smtpTransport = nodemailer.createTransport({
+            service: 'Gmail', 
+            auth: {
+              user: 'NicePointInfo@gmail.com',
+              pass: 'wwk4652218'
+            }
+          });
+          var mailOptions = {
+            to: user.email,
+            from: 'NicePointInfo@gmail.com',
+            subject: '您的密码已经被修改',
+            text: '您好,\n\n' +
+              '您账户 ' + user.email + ' 的密码已经被修改。\n'
+          };
+          smtpTransport.sendMail(mailOptions, function(err) {
+            req.flash('success', '密码修改成功');
+            done(err);
+          });
+        }
+      ], function(err) {
+        res.redirect('/campgrounds');
+      });
+    });
 
 // User's profile
 router.get("/users/:id", function(req, res){
